e921a22eddd4e6fd4dbf254cc2d10e43d1ca6bd5
Sultan2403 <sultanremilekun@gmail.com>
2026-01-28 13:37:39 +0100

    fixed up file structure a bit

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+app.use(express.json())
 
 app.get("/health", (req, res) => {
   res.send("Server says heyyy :)");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app
