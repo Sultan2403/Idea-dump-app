bca5dffb468d91116eeb7b823e27fe365232ba67
Sultan2403 <sultanremilekun@gmail.com>
2026-02-01 22:18:05 +0100

    before edit component update

--- Frontend/src/Apis/api.client.js
+++ Frontend/src/Apis/api.client.js
@@ -4,7 +4,7 @@ const url = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: url,
-  timeout: url.includes("localhost") ? 5000 : 15000,
+  timeout: url.includes("localhost") ? 10000 : 30000,
 });
 
 api.interceptors.response.use(
